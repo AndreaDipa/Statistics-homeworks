05282c85959178b3ab5ea68a980662768f70fb0a
AndreaDipa <andreadipaa@gmail.com>
2022-10-03 16:46:26 +0200

    CSharp

---
+++ Homework1/CSharp/MyVeryFirstCSharpProgram/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace MyVeryFirstCSharpProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(13, 36);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 81);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dispaly Text";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.Color.Green;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(13, 146);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 55);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Increase";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(203, 36);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(537, 81);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(203, 146);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(537, 55);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Value = 50;
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(104, 146);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 55);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Decrease";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(13, 208);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Data";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(537, 300);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(579, 226);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 55);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "+";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Red;
+            this.button5.Location = new System.Drawing.Point(670, 226);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 55);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "-";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Blue;
+            this.button6.Location = new System.Drawing.Point(579, 287);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(85, 55);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "+";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Blue;
+            this.button7.Location = new System.Drawing.Point(670, 287);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(85, 55);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "-";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Green;
+            this.button8.Location = new System.Drawing.Point(579, 348);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(85, 55);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "+";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Green;
+            this.button9.Location = new System.Drawing.Point(670, 348);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(85, 55);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "-";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Purple;
+            this.button10.Location = new System.Drawing.Point(670, 409);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(85, 55);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "-";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.Purple;
+            this.button11.Location = new System.Drawing.Point(579, 409);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(85, 55);
+            this.button11.TabIndex = 14;
+            this.button11.Text = "+";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(558, 245);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(558, 306);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(558, 367);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 16);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(558, 418);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 16);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(922, 520);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "My First C# App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Homework1/CSharp/MyVeryFirstCSharpProgram/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace MyVeryFirstCSharpProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Series["Data"].Points.AddXY("1", 0);
+            chart1.Series["Data"].Points[0].Color = Color.Red;
+            chart1.Series["Data"].Points.AddXY("2", 0);
+            chart1.Series["Data"].Points[1].Color = Color.Blue;
+            chart1.Series["Data"].Points.AddXY("3", 0);
+            chart1.Series["Data"].Points[2].Color = Color.Green;
+            chart1.Series["Data"].Points.AddXY("4", 0);
+            chart1.Series["Data"].Points[3].Color = Color.Purple;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Hello World!";
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "The mouse entered button1!";
+        }
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "The mouse left button1!";
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < 100)
+            {
+                progressBar1.Value += 10;
+                if (progressBar1.Value == 10)
+                    button3.Enabled = true;
+                else if (progressBar1.Value == 100)
+                    button2.Enabled = false;
+
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (progressBar1.Value > 0)
+            {
+                progressBar1.Value -= 10;
+                if (progressBar1.Value == 90)
+                    button2.Enabled = true;
+                else if (progressBar1.Value == 0)
+                    button3.Enabled = false;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            chart1.Series["Data"].Points[0].YValues[0] += 1;
+            chart1.ChartAreas[0].RecalculateAxesScale();
+            chart1.Refresh();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            chart1.Series["Data"].Points[0].YValues[0] -= 1;
+            chart1.ChartAreas[0].RecalculateAxesScale();
+            chart1.Refresh();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            chart1.Series["Data"].Points[1].YValues[0] += 1;
+            chart1.ChartAreas[0].RecalculateAxesScale();
+            chart1.Refresh();
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            chart1.Series["Data"].Points[1].YValues[0] -= 1;
+            chart1.ChartAreas[0].RecalculateAxesScale();
+            chart1.Refresh();
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            chart1.Series["Data"].Points[2].YValues[0] += 1;
+            chart1.ChartAreas[0].RecalculateAxesScale();
+            chart1.Refresh();
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            chart1.Series["Data"].Points[2].YValues[0] -= 1;
+            chart1.ChartAreas[0].RecalculateAxesScale();
+            chart1.Refresh();
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            chart1.Series["Data"].Points[3].YValues[0] -= 1;
+            chart1.ChartAreas[0].RecalculateAxesScale();
+            chart1.Refresh();
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+            chart1.Series["Data"].Points[3].YValues[0] += 1;
+            chart1.ChartAreas[0].RecalculateAxesScale();
+            chart1.Refresh();
+        }
+    }
+}
